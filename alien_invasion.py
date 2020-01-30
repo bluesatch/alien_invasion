65abceefb4f1e50cfa721386bf95b91f8cc7b6c3
H Brown <hebrown12@gmail.com>
2020-01-30 15:22:26 -0600

    created media folder; downloaded ship; tested game screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,19 +4,23 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 """
 First, we import the sys and pygame modules. The pygame module con- tains the functionality needed to make a game. We’ll use the sys module to exit the game when the player quits.
 
 """
 
 def run_game():
-    # Initialize game and create a screen object
+    # Initialize pygame, settings, and screen object
     pygame.init()
 
     # The line pygame.init() initializes background settings that Pygame needs to work properly
 
+    ai_settings = Settings()
 
-    screen = pygame.display.set_mode((1200,800)) # a tuple
+
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height)) # a tuple
 
     # we call pygame.display.set_mode() to create a display window called screen, on which we’ll draw all of the game’s graphical elements
 
@@ -25,7 +29,7 @@ def run_game():
 
     # Set the background color
 
-    bg_color = (230, 230, 230)
+#     bg_color = (230, 230, 230)
     # colors in pygame are in rgb
 
     # Set main loop
@@ -34,7 +38,7 @@ def run_game():
 
         # Watch for keyboard and mouse events
 
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
